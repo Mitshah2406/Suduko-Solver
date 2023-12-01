722a2cec3a7f1ae817f41d608405f5e15aa08f40
mitshah2406 <mitshah2406@gmail.com>
2023-12-01 17:51:46 +0530

    merged in one file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import tkinter as tk
-import solverLogic
+
 
 
 def ValidateNumber(P):
@@ -78,7 +78,35 @@ def button_click01(button_text):
 def clear_entry():
     for b, a in cells.items():
         a.delete(0, tk.END)
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
 
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, i, j, num):
+                        board[i][j] = num
+
+                        if solve_sudoku(board):
+                            return True
+
+                        board[i][j] = 0
+
+                return False
+
+    return True
 def solve_sudoku_ui():
     board = []
     for row in range(2, 11):
@@ -91,7 +119,7 @@ def solve_sudoku_ui():
                 rows.append(int(val))
         board.append(rows)
 
-    if solverLogic.solve_sudoku(board):
+    if solve_sudoku(board):
         for row in range(2, 11):
             for col in range(1, 10):
                 cells[(row, col)].delete(0, END)
